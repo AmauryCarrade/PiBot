78e4004b640198f085d3f4fb58c44998f42060eb
Amaury Carrade <amaury@carrade.eu>
2015-02-05 16:31:06 +0100

    Fixed duplicated hook ID

--- pibot/Hook.py
+++ pibot/Hook.py
@@ -12,7 +12,7 @@ class Hook(Enum):
 	CHANNEL_MESSAGE_RECEIVED = "hook.received.channel_message"
 
 	# Fired when a private message is sent to the bot.
-	PRIVATE_MESSAGE_RECEIVED = "hook.received.channel_message"
+	PRIVATE_MESSAGE_RECEIVED = "hook.received.private_message"
 
 	# Fired when the bot receive a message either from the channel or a specific user (private message).
 	# Do not fires for CTCP requests.
